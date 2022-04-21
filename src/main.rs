333f3c53569b8a764aebc1d980f048f6a4a39e81
TodRieter <thomas@wilson.land>
2022-04-21 02:39:03 -0500

    add fixed_point_arithmatic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 enum Expression {
     Integer(i32),
-    FixedPoint(i32,i32),
-    Addition(Vec<Expression>)
+    FixedPoint(i32, i32),
+    Addition(Vec<Expression>),
 }
 
-
-fn evaluate_integer(expression: &Expression)->f64 {
+fn evaluate_integer(expression: &Expression) -> f64 {
     if let Expression::Integer(value) = expression {
         *value as f64
     } else {
@@ -13,12 +12,50 @@ fn evaluate_integer(expression: &Expression)->f64 {
     }
 }
 
+fn evaluate_fixed_point(expression: &Expression) -> f64 {
+    if let Expression::FixedPoint(whole_part, decimal_part) = expression {
+        // if the decimal part is greater than or equal to 10
+        if *decimal_part as f64 >= 10.0 {
+            // value = whole_part + decimal_part/(10^(number of didgets in the decimal part + 1))))
+            let value = *whole_part as f64
+                + (*decimal_part as f64)
+                    / ((10.0 as f64).powi((*decimal_part as f64).log10() as i32 + 1));
+            value as f64
+        } else {
+            // if the decimal_part < 10 return the whole_part + decimal part/100
+            let value = ((*whole_part * 100) + *decimal_part) as f64 / 100.0;
+            value
+        }
+    } else {
+        panic!("not a fixed_point, HOW DID YOU EVEN GET HERE! ARE YOU A JUNIOR PROGRAMMER????");
+    }
+}
+
+fn evaluate_add_fixed_point(expressions: &Vec<Expression>) -> Expression {
+    let mut total = 0.0;
+
+    for each in expressions {
+        if let Expression::FixedPoint(whole_part, decimal_part) = each {
+            total +=
+                evaluate_fixed_point(&Expression::FixedPoint(*whole_part, *decimal_part)) as f64;
+        } else {
+            panic!("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH! that wasn't an fixedpoint. I can only add fixedpoints!")
+        }
+    }
+    Expression::FixedPoint(
+        // the whole_part is just the total truncated
+        total.floor() as i32,
+        // the decimal part is two didgets multiply by the total 100 and get the last two didgets
+        (total * 100.0) as i32 % 100,
+    )
+}
 
 fn evaluate_addition(expressions: &Expression) -> Expression {
     if let Expression::Addition(expressions) = expressions {
         match expressions[0] {
+            Expression::FixedPoint(_, _) => evaluate_add_fixed_point(expressions),
             Expression::Integer(_) => evaluate_add_integers(expressions),
-            _ => panic!("I only konw how to add integers")
+            _ => panic!("I only know how to add integers"),
         }
     } else {
         panic!("How could you do this to me? I expected an addition :(");
@@ -37,17 +74,14 @@ fn evaluate_add_integers(expressions: &Vec<Expression>) -> Expression {
     Expression::Integer(total)
 }
 
-
-fn evaluate(expression: &Expression)->f64 {
+fn evaluate(expression: &Expression) -> f64 {
     match expression {
         Expression::Integer(_) => evaluate_integer(expression),
+        Expression::FixedPoint(_, _) => evaluate_fixed_point(expression),
         Expression::Addition(_) => evaluate(&evaluate_addition(expression)),
-
-        _=> panic!("not implemented")
+        _ => panic!("not implemented"),
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -59,23 +93,80 @@ mod tests {
     fn test_integer_value() {
         let expr = crate::Expression::Integer(42);
         let value = crate::evaluate(&expr);
-        assert_eq!(value,42.0, "should be equal, 42 and 42.0");
+        assert_eq!(
+            value, 42.0,
+            "should be equal, 42 and 42.0 instead value = {}",
+            value
+        );
+    }
 
+    #[test]
+    fn test_fixed_point_value() {
+        let expr = crate::Expression::FixedPoint(4, 20);
+        let value = crate::evaluate(&expr);
+        assert_eq!(value, 4.2, "should be equal, 4.2 and 4.20");
     }
 
     #[test]
     fn test_simple_addition() {
         let expr = crate::Expression::Addition(vec![
             crate::Expression::Integer(2),
-            crate::Expression::Integer(2)
+            crate::Expression::Integer(2),
         ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(4.0, value);
+    }
+
+    #[test]
+    fn test_fixed_point_addition_0() {
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(1, 20), // trailing 0 is required
+            crate::Expression::FixedPoint(1, 20),
+        ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(2.4, value, "2.4 expected, value = {}", value);
+    }
+
+    #[test]
+    fn test_fixed_point_addition_1() {
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(1, 20),
+            crate::Expression::FixedPoint(1, 22),
+        ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(2.42, value, "2.22 expected, value = {}", value);
+    }
+
+    #[test]
+    fn test_fixed_point_addition_2() {
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(3, 1415926535),
+            crate::Expression::FixedPoint(3, 1415926535),
+        ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(6.28, value, "6.28 expected, value = {}", value);
+    }
+
+    #[test]
+    fn test_fixed_point_addition_3() {
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(0, 0),
+            crate::Expression::FixedPoint(0, 0),
+        ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(0.0, value, "0.0 expected, value = {}", value);
+    }
+
+    #[test]
+    fn test_fixed_point_addition_4() {
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(1, 02), //the leading 0 is not required
+            crate::Expression::FixedPoint(1, 2),  // proof of the statement above
+        ]);
+        let value = crate::evaluate(&expr);
+        assert_eq!(2.04, value, "2.04 expected, value = {}", value);
     }
 }
-
-
-
-
-
 fn main() {
     println!("Hello, world!");
 }
